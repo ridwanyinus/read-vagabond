47c910b57386ee6418bb3597e08b3d23b17fca22
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-19 15:56:58 -0500

    chore: use new scans

--- manga/worker/src/index.ts
+++ manga/worker/src/index.ts
@@ -2,14 +2,42 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 
-		// http://localhost:8087/volume-XY/cover.jpg
-		const match_volume = url.pathname.match(/volume-(\d+)\/cover\.jpg$/);
+		// http://localhost:8087/volume-XY/chapter-X/page-XYZ
+		const match_page = url.pathname.match(/volume-(\d+)\/chapter-(\d+)\/page-(\d+)$/);
+		if (match_page) {
+			const volume = match_page[1];
+			const chapter = match_page[2];
+			const page = match_page[3];
 
-		// http://localhost:8087/chapter-X/page-XYZ.png
-		const match_page = url.pathname.match(/chapter-(\d+)\/page-(\d+)\.png$/);
+			const object_png = await env.vagabond_manga_vizbig.get(`volume-${volume}/chapter-${chapter}/page-${page}.png`);
+			if (object_png) {
+				return new Response(object_png.body, {
+					headers: {
+						'Content-Type': 'image/png',
+						'Cache-Control': 'public, max-age=31536000, immutable',
+						'Access-Control-Allow-Origin': '*',
+					},
+				});
+			}
 
-		if (match_volume) {
-			const volume = match_volume[1];
+			const object_jpg = await env.vagabond_manga_vizbig.get(`volume-${volume}/chapter-${chapter}/page-${page}.jpg`);
+			if (object_jpg) {
+				return new Response(object_jpg.body, {
+					headers: {
+						'Content-Type': 'image/jpg',
+						'Cache-Control': 'public, max-age=31536000, immutable',
+						'Access-Control-Allow-Origin': '*',
+					},
+				});
+			}
+
+			return new Response('Not Found', { status: 404 });
+		}
+
+		// http://localhost:8087/volume-XY/cover
+		const match_cover = url.pathname.match(/volume-(\d+)\/cover$/);
+		if (match_cover) {
+			const volume = match_cover[1];
 
 			const object = await env.vagabond_manga_hq.get(`volume-${volume}/cover.jpg`);
 			if (!object) {
@@ -23,24 +51,8 @@ export default {
 					'Access-Control-Allow-Origin': '*',
 				},
 			});
-		} else if (match_page) {
-			const chapter = match_page[1];
-			const page = match_page[2];
-
-			const object = await env.manga.get(`chapter-${chapter}/page-${page}.png`);
-			if (!object) {
-				return new Response('Not Found', { status: 404 });
-			}
-
-			return new Response(object.body, {
-				headers: {
-					'Content-Type': 'image/png',
-					'Cache-Control': 'public, max-age=31536000, immutable',
-					'Access-Control-Allow-Origin': '*',
-				},
-			});
-		} else {
-			return new Response('Not Found', { status: 404 });
 		}
+
+		return new Response('Not Found', { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;

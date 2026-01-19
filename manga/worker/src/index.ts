ab0d08275edfb6d373f1eed02dab75f33e74a815
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-19 11:51:56 -0500

    feat: show volumes

--- manga/worker/src/index.ts
+++ manga/worker/src/index.ts
@@ -2,26 +2,45 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 
+		// http://localhost:8087/volume-XY/cover.jpg
+		const match_volume = url.pathname.match(/volume-(\d+)\/cover\.jpg$/);
+
 		// http://localhost:8087/chapter-X/page-XYZ.png
-		const match = url.pathname.match(/chapter-(\d+)\/page-(\d+)\.png$/);
-		if (!match) {
+		const match_page = url.pathname.match(/chapter-(\d+)\/page-(\d+)\.png$/);
+
+		if (match_volume) {
+			const volume = match_volume[1];
+
+			const object = await env.vagabond_manga_hq.get(`volume-${volume}/cover.jpg`);
+			if (!object) {
+				return new Response('Not Found', { status: 404 });
+			}
+
+			return new Response(object.body, {
+				headers: {
+					'Content-Type': 'image/jpg',
+					'Cache-Control': 'public, max-age=31536000, immutable',
+					'Access-Control-Allow-Origin': '*',
+				},
+			});
+		} else if (match_page) {
+			const chapter = match_page[1];
+			const page = match_page[2];
+
+			const object = await env.manga.get(`chapter-${chapter}/page-${page}.png`);
+			if (!object) {
+				return new Response('Not Found', { status: 404 });
+			}
+
+			return new Response(object.body, {
+				headers: {
+					'Content-Type': 'image/png',
+					'Cache-Control': 'public, max-age=31536000, immutable',
+					'Access-Control-Allow-Origin': '*',
+				},
+			});
+		} else {
 			return new Response('Not Found', { status: 404 });
 		}
-
-		const chapter = match[1];
-		const page = match[2];
-
-		const object = await env.manga.get(`chapter-${chapter}/page-${page}.png`);
-		if (!object) {
-			return new Response('Not Found', { status: 404 });
-		}
-
-		return new Response(object.body, {
-			headers: {
-				'Content-Type': 'image/png',
-				'Cache-Control': 'public, max-age=31536000, immutable',
-				'Access-Control-Allow-Origin': '*',
-			},
-		});
 	},
 } satisfies ExportedHandler<Env>;

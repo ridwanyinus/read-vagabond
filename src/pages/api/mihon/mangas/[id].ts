763453b52145ffffb4573a49478aef5c7b57622b
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-20 14:18:08 -0500

    api: send chapter metadata too

--- src/pages/api/mihon/mangas/[id].ts
+++ src/pages/api/mihon/mangas/[id].ts
@@ -2,17 +2,23 @@
 import type { APIRoute } from "astro";
 import data from "../data.json";
 
-export const GET: APIRoute = async ({ params }) => {
+export const GET: APIRoute = async ({ locals, params }) => {
   const { id } = params;
 
-  if (!id || Number(id) > data.length) {
+  const manga = data.find((manga) => manga.id === id);
+  if (!manga) {
     return new Response(JSON.stringify({ error: "Manga not found" }), {
       status: 404,
       headers: { "Content-Type": "application/json" },
     });
   }
 
-  return new Response(JSON.stringify(data.find((manga) => manga.id === id)), {
+  const db = locals.runtime?.env?.vagabond_db;
+  const { results: chapters } = await db
+    .prepare("SELECT * FROM chapters ORDER BY number ASC")
+    .all();
+
+  return new Response(JSON.stringify({ ...manga, chapters }), {
     headers: {
       "Content-Type": "application/json",
       "Cache-Control":

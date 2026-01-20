d03a6c992f20063f178443fcf71234e00ed228b3
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-20 16:24:53 -0500

    api: update to support query param

--- src/pages/api/mihon/mangas.ts
+++ src/pages/api/mihon/mangas.ts
@@ -2,8 +2,21 @@
 import type { APIRoute } from "astro";
 import data from "./data.json";
 
-export const GET: APIRoute = async () => {
-  return new Response(JSON.stringify(data), {
+export const GET: APIRoute = async ({ url }) => {
+  let filteredData = data;
+  const searchQuery = url.searchParams.get("q")?.toLocaleLowerCase();
+  if (searchQuery) {
+    filteredData = data.filter((manga) =>
+      manga.title.toLocaleLowerCase().includes(searchQuery),
+    );
+  }
+
+  // For possible pagination in the future
+  // const startIndex = (page - 1) * pageSize;
+  // const endIndex = startIndex + pageSize;
+  // const paginatedData = filteredData.slice(startIndex, endIndex);
+
+  return new Response(JSON.stringify(filteredData), {
     headers: {
       "Content-Type": "application/json",
       "Cache-Control":

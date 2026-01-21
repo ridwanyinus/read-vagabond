d81d87a26704d0ccb9a544e45d06fd5e34ae95b1
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-21 01:21:58 -0500

    chore: manually cache manga scans

--- manga/worker/src/index.ts
+++ manga/worker/src/index.ts
@@ -2,6 +2,19 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 
+		const canCache = request.method === 'GET';
+		if (canCache) {
+			let response = await caches.default.match(request);
+			if (response) {
+				const newHeaders = new Headers(response.headers);
+				return new Response(response.body, {
+					status: response.status,
+					statusText: response.statusText,
+					headers: newHeaders,
+				});
+			}
+		}
+
 		// http://localhost:8087/volume-XY/chapter-X/page-XYZ
 		const match_page = url.pathname.match(/volume-(\d+)\/chapter-(\d+)\/page-(\d+)$/);
 		if (match_page) {
@@ -11,24 +24,29 @@ export default {
 
 			const object_png = await env.vagabond_manga_vizbig.get(`volume-${volume}/chapter-${chapter}/page-${page}.png`);
 			if (object_png) {
-				return new Response(object_png.body, {
+				const response = new Response(object_png.body, {
 					headers: {
 						'Content-Type': 'image/png',
 						'Cache-Control': 'public, max-age=31536000, s-maxage=31536000, stale-while-revalidate=31536000, immutable',
 						'Access-Control-Allow-Origin': '*',
 					},
 				});
+				if (canCache) ctx.waitUntil(caches.default.put(request, response.clone()));
+
+				return response;
 			}
 
 			const object_jpg = await env.vagabond_manga_vizbig.get(`volume-${volume}/chapter-${chapter}/page-${page}.jpg`);
 			if (object_jpg) {
-				return new Response(object_jpg.body, {
+				const response = new Response(object_jpg.body, {
 					headers: {
 						'Content-Type': 'image/jpeg',
 						'Cache-Control': 'public, max-age=31536000, s-maxage=31536000, stale-while-revalidate=31536000, immutable',
 						'Access-Control-Allow-Origin': '*',
 					},
 				});
+				if (canCache) ctx.waitUntil(caches.default.put(request, response.clone()));
+				return response;
 			}
 
 			return new Response('Not Found', { status: 404 });
@@ -44,13 +62,15 @@ export default {
 				return new Response('Not Found', { status: 404 });
 			}
 
-			return new Response(object.body, {
+			const response = new Response(object.body, {
 				headers: {
 					'Content-Type': 'image/jpeg',
 					'Cache-Control': 'public, max-age=31536000, s-maxage=31536000, stale-while-revalidate=31536000, immutable',
 					'Access-Control-Allow-Origin': '*',
 				},
 			});
+			if (canCache) ctx.waitUntil(caches.default.put(request, response.clone()));
+			return response;
 		}
 
 		return new Response('Not Found', { status: 404 });

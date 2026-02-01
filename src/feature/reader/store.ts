8b15a4ccc917ab29a895a9dc60e34c659b49e4e1
Leonardo <leonardo.estacio.h@gmail.com>
2026-02-01 13:39:34 -0500

    feat: use nanostores for state management across components

---
+++ src/feature/reader/store.ts
@@ -0,0 +1,5 @@
+import { atom } from "nanostores";
+
+export const currentPage = atom(0);
+export const percentageRead = atom(0);
+export const navbarVisibility = atom<"visible" | "hidden">("visible");

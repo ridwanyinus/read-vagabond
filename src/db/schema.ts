22be447defce584fe61fb0aad9b0fbb431c29766
Leonardo <leonardo.estacio.h@gmail.com>
2026-02-03 20:55:18 -0500

    feat: generate database schema migration

---
+++ src/db/schema.ts
@@ -0,0 +1,52 @@
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const authorsTable = sqliteTable("authors", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const mangasTable = sqliteTable("mangas", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  authorId: integer("author_id")
+    .notNull()
+    .references(() => authorsTable.id),
+  artistId: integer("artist_id")
+    .notNull()
+    .references(() => authorsTable.id),
+  title: text("title").notNull(),
+  description: text("description"),
+  status: text("status").notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const volumesTable = sqliteTable("volumes", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  mangaId: integer("manga_id")
+    .notNull()
+    .references(() => mangasTable.id),
+  number: integer("number").notNull(),
+  releaseDate: integer("release_date", { mode: "timestamp" }),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
+
+export const chaptersTable = sqliteTable("chapters", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  volumeId: integer("volume_id")
+    .notNull()
+    .references(() => volumesTable.id),
+  title: text("title").notNull(),
+  number: integer("number").notNull(),
+  pageCount: integer("page_count").notNull(),
+  releaseDate: integer("release_date", { mode: "timestamp" }),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
